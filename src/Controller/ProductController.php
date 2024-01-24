6d27039c57ad3db8acf774958479fcee00aac143
MatheusLBrum <matheusdelimabrum@gmail.com>
2024-01-24 16:18:53 -0300

    fixing products with category null on individual search

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -22,7 +22,7 @@ class ProductController
     public function getAll(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
         $adminUserId = $request->getHeader('admin_user_id')[0];
-        
+
         $stm = $this->service->getAll($adminUserId);
         $response->getBody()->write(json_encode($stm->fetchAll()));
         return $response->withStatus(200);
