9e80204441f1b2f3b011a865c1f5d4911016763e
MatheusLBrum <matheusdelimabrum@gmail.com>
2024-01-25 23:31:28 -0300

    criando função para filtrar e solicitar produtos

---
+++ src/utils/filterFunction.php
@@ -0,0 +1,38 @@
+<?php
+
+function getFilteredProducts($stm, $order, $status, $category)
+{
+  if ($order || $status || $category) {
+    $newStm = $stm;
+
+    if ($order) {
+      usort($newStm, function ($productA, $productB) use ($order) {
+        $dateA = strtotime($productA->created_at);
+        $dateB = strtotime($productB->created_at);
+
+        if ($order === 'asc') {
+          return $dateA <=> $dateB;
+        } else {
+          return $dateB <=> $dateA;
+        }
+      });
+    }
+
+    if ($category) {
+      $newStm = array_filter($newStm, function ($product) use ($category) {
+        return $product->category == $category;
+      });
+    }
+
+    if ($status) {
+      $newStm = array_filter($newStm, function ($produto) use ($status) {
+        return $produto->active == ($status === "active" ? 1 : 0);
+      });
+      $newStm = array_values($newStm);
+    }
+
+    return $newStm;
+  } else {
+    return $stm;
+  }
+}
